a4eefd07cfb403a3ea5f61e51f9462f514b8f100
Erik Öjebo <erik.ojebo@gmail.com>
2011-12-29 19:26:21 +0100

    GH-21 Finished removing duplication between dynamic builders

--- src/WeenyMapper/QueryBuilding/DynamicUpdateBuilder.cs
+++ src/WeenyMapper/QueryBuilding/DynamicUpdateBuilder.cs
@@ -6,10 +6,8 @@ using WeenyMapper.QueryExecution;
 
 namespace WeenyMapper.QueryBuilding
 {
-    public class DynamicUpdateBuilder<T> : DynamicObject
+    public class DynamicUpdateBuilder<T> : DynamicCommandBuilderBase
     {
-        private readonly IDictionary<string, object> _constraints = new Dictionary<string, object>();
-        private readonly IDictionary<string, object> _setters = new Dictionary<string, object>();
         private readonly IObjectUpdateExecutor _objectUpdateExecutor;
 
         public DynamicUpdateBuilder(IObjectUpdateExecutor objectUpdateExecutor)
@@ -22,32 +20,17 @@ namespace WeenyMapper.QueryBuilding
             _objectUpdateExecutor.Update(instance);
         }
 
-        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        protected override IEnumerable<string> ValidPrefixes
         {
-            var calledMethodName = binder.Name;
-
-            if (calledMethodName.StartsWith("Where"))
-            {
-                var propertyName = calledMethodName.Substring("Where".Length);
-                _constraints[propertyName] = args[0];                
-            }
-            else if (calledMethodName.StartsWith("Set"))
-            {
-                var propertyName = calledMethodName.Substring("Set".Length);
-                _setters[propertyName] = args[0];                
-            }
-            else
-            {
-                throw new InvalidOperationException("Unknown prefix");    
-            }
-
-            result = this;
-            return true;
+            get { return new[] { "Where", "Set" }; }
         }
 
         public void Execute()
         {
-            _objectUpdateExecutor.Update<T>(_constraints, _setters);
+            var constraints = GetPropertyValues("Where");
+            var setters = GetPropertyValues("Set");
+
+            _objectUpdateExecutor.Update<T>(constraints, setters);
         }
     }
 }
