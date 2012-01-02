31e6335d7627819babf4e362a1f89783716aa568
Erik Öjebo <erik.ojebo@gmail.com>
2012-01-02 15:16:17 +0100

    GH-28 Refactored acceptance tests for async stuff

--- src/WeenyMapper.Specs/AsyncRepositoryAcceptanceSpecs.cs
+++ src/WeenyMapper.Specs/AsyncRepositoryAcceptanceSpecs.cs
@@ -31,19 +31,14 @@ namespace WeenyMapper.Specs
         [Test]
         public void Insert_for_single_entity_can_be_run_asynchronously()
         {
-            var wasCallbackCalledSemaphore = new Semaphore(0, 1);
-
             var user = new User
-            {
-                Id = Guid.NewGuid(),
-                Username = "a username",
-                Password = "a password"
-            };
+                {
+                    Id = Guid.NewGuid(),
+                    Username = "a username",
+                    Password = "a password"
+                };
 
-            Repository.InsertAsync(user, () => wasCallbackCalledSemaphore.Release(1));
-
-            // Wait until callback is called. The test will fail if the timeout is reached)
-            wasCallbackCalledSemaphore.WaitOne();
+            AssertCallbackIsInvoked(callback => Repository.InsertAsync(user, callback));
 
             var actualUser = Repository.Find<User>().Where(x => x.Id, user.Id).Execute();
             Assert.AreEqual(user, actualUser);
@@ -53,26 +48,21 @@ namespace WeenyMapper.Specs
         [Test]
         public void Insert_for_multiple_entities_can_be_run_asynchronously()
         {
-            var wasCallbackCalledSemaphore = new Semaphore(0, 1);
-
             var user1 = new User
-            {
-                Id = Guid.NewGuid(),
-                Username = "username1",
-                Password = "a password"
-            };
+                {
+                    Id = Guid.NewGuid(),
+                    Username = "username1",
+                    Password = "a password"
+                };
 
             var user2 = new User
-            {
-                Id = Guid.NewGuid(),
-                Username = "username2",
-                Password = "a password"
-            };
+                {
+                    Id = Guid.NewGuid(),
+                    Username = "username2",
+                    Password = "a password"
+                };
 
-            Repository.InsertManyAsync(new[] { user1, user2 }, () => wasCallbackCalledSemaphore.Release());
-
-            // Wait until callback is called. The test will fail if the timeout is reached)
-            wasCallbackCalledSemaphore.WaitOne();
+            AssertCallbackIsInvoked(callback => Repository.InsertManyAsync(new[] { user1, user2 }, callback));
 
             var actualUsers = Repository.Find<User>().ExecuteList();
 
@@ -85,23 +75,18 @@ namespace WeenyMapper.Specs
         [Test]
         public void Update_of_single_entity_can_be_run_asynchronously()
         {
-            var wasCallbackCalledSemaphore = new Semaphore(0, 1);
-
             var user1 = new User
-            {
-                Id = Guid.NewGuid(),
-                Username = "username1",
-                Password = "a password"
-            };
+                {
+                    Id = Guid.NewGuid(),
+                    Username = "username1",
+                    Password = "a password"
+                };
 
             Repository.Insert(user1);
 
             user1.Username = "Updated username";
 
-            Repository.UpdateAsync(user1, () => wasCallbackCalledSemaphore.Release());
-
-            // Wait until callback is called. The test will fail if the timeout is reached)
-            wasCallbackCalledSemaphore.WaitOne();
+            AssertCallbackIsInvoked(callback => Repository.UpdateAsync(user1, callback));
 
             var actualUser = Repository.Find<User>().Where(x => x.Id, user1.Id).Execute();
 
@@ -116,44 +101,39 @@ namespace WeenyMapper.Specs
             var actualRowCount = 0;
 
             var user1 = new User
-            {
-                Id = Guid.NewGuid(),
-                Username = "username1",
-                Password = "a password"
-            };
+                {
+                    Id = Guid.NewGuid(),
+                    Username = "username1",
+                    Password = "a password"
+                };
 
             var user2 = new User
-            {
-                Id = Guid.NewGuid(),
-                Username = "username2",
-                Password = "another password"
-            };
+                {
+                    Id = Guid.NewGuid(),
+                    Username = "username2",
+                    Password = "another password"
+                };
 
             var user3 = new User
-            {
-                Id = Guid.NewGuid(),
-                Username = "username3",
-                Password = "a password"
-            };
+                {
+                    Id = Guid.NewGuid(),
+                    Username = "username3",
+                    Password = "a password"
+                };
 
             Repository.InsertMany(user1, user2, user3);
 
-            Repository.Update<User>().Where(x => x.Password, "a password")
-                .Set(x => x.Password, "updated password")
-                .ExecuteAsync(rowCount =>
-                {
-                    actualRowCount = rowCount;
-                    wasCallbackCalledSemaphore.Release();
-                });
-
-            // Wait until callback is called. The test will fail if the timeout is reached)
-            wasCallbackCalledSemaphore.WaitOne();
+            AssertParameterizedCallbackIsInvoked(2,
+                callback =>
+                Repository.Update<User>()
+                    .Where(x => x.Password, "a password")
+                    .Set(x => x.Password, "updated password")
+                    .ExecuteAsync(callback));
 
             var actualUsers = Repository.Find<User>()
                 .Where(x => x.Password, "updated password")
                 .ExecuteList();
 
-            Assert.AreEqual(2, actualRowCount);
             Assert.AreEqual(2, actualUsers.Count);
             Assert.AreEqual("updated password", actualUsers[0].Password);
             Assert.AreEqual("updated password", actualUsers[1].Password);
@@ -163,23 +143,18 @@ namespace WeenyMapper.Specs
         [Test]
         public void Delete_of_single_entity_can_be_run_asynchronously()
         {
-            var wasCallbackCalledSemaphore = new Semaphore(0, 1);
-
             var user1 = new User
-            {
-                Id = Guid.NewGuid(),
-                Username = "username1",
-                Password = "a password"
-            };
+                {
+                    Id = Guid.NewGuid(),
+                    Username = "username1",
+                    Password = "a password"
+                };
 
             Repository.Insert(user1);
 
             user1.Username = "Updated username";
 
-            Repository.DeleteAsync(user1, () => wasCallbackCalledSemaphore.Release());
-
-            // Wait until callback is called. The test will fail if the timeout is reached)
-            wasCallbackCalledSemaphore.WaitOne();
+            AssertCallbackIsInvoked(callback => Repository.DeleteAsync(user1, callback));
 
             var actualUserCount = Repository.Count<User>().Execute();
 
@@ -194,42 +169,64 @@ namespace WeenyMapper.Specs
             var actualRowCount = 0;
 
             var user1 = new User
-            {
-                Id = Guid.NewGuid(),
-                Username = "username1",
-                Password = "a password"
-            };
+                {
+                    Id = Guid.NewGuid(),
+                    Username = "username1",
+                    Password = "a password"
+                };
 
             var user2 = new User
-            {
-                Id = Guid.NewGuid(),
-                Username = "username2",
-                Password = "another password"
-            };
+                {
+                    Id = Guid.NewGuid(),
+                    Username = "username2",
+                    Password = "another password"
+                };
 
             var user3 = new User
-            {
-                Id = Guid.NewGuid(),
-                Username = "username3",
-                Password = "a password"
-            };
+                {
+                    Id = Guid.NewGuid(),
+                    Username = "username3",
+                    Password = "a password"
+                };
 
             Repository.InsertMany(user1, user2, user3);
 
-            Repository.Delete<User>().Where(x => x.Password, "a password")
-                .ExecuteAsync(rowCount =>
+            AssertParameterizedCallbackIsInvoked(2,
+                callback =>
+                Repository.Delete<User>()
+                    .Where(x => x.Password, "a password")
+                    .ExecuteAsync(callback));
+
+            var userCount = Repository.Count<User>().Execute();
+
+            Assert.AreEqual(1, userCount);
+        }
+
+        private void AssertCallbackIsInvoked(Action<Action> operation)
+        {
+            var wasCallbackCalledSemaphore = new Semaphore(0, 1);
+
+            operation(() => wasCallbackCalledSemaphore.Release());
+
+            // Wait until callback is called. The test will fail if the timeout is reached)
+            wasCallbackCalledSemaphore.WaitOne();
+        }
+
+        private void AssertParameterizedCallbackIsInvoked<T>(T expectedParameter, Action<Action<T>> operation)
+        {
+            var actualParameter = default(T);
+            var wasCallbackCalledSemaphore = new Semaphore(0, 1);
+
+            operation(parameter =>
                 {
-                    actualRowCount = rowCount;
+                    actualParameter = parameter;
                     wasCallbackCalledSemaphore.Release();
                 });
 
             // Wait until callback is called. The test will fail if the timeout is reached)
             wasCallbackCalledSemaphore.WaitOne();
 
-            var userCount = Repository.Count<User>().Execute();
-
-            Assert.AreEqual(2, actualRowCount);
-            Assert.AreEqual(1, userCount);
+            Assert.AreEqual(expectedParameter, actualParameter);
         }
     }
 }
