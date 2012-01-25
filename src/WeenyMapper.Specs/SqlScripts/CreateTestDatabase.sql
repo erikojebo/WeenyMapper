4d80a33121b1b928a10404d33f7c86f078792d8c
Erik Öjebo <erik.ojebo@gmail.com>
2012-01-25 13:43:37 +0100

    GH-42 Added test tables to SQL script

--- src/WeenyMapper.Specs/SqlScripts/CreateTestDatabase.sql
+++ src/WeenyMapper.Specs/SqlScripts/CreateTestDatabase.sql
@@ -2,7 +2,6 @@
 	[Id] [uniqueidentifier] NOT NULL PRIMARY KEY,
 	[Username] [nvarchar](255) NOT NULL,
 	[Password] [nvarchar](255) NOT NULL)
-
 GO
 
 CREATE TABLE [t_Books](
@@ -10,7 +9,6 @@ CREATE TABLE [t_Books](
 	[c_TITLE] [nvarchar](255) NOT NULL,
 	[c_AUTHORNAME] [nvarchar](255) NOT NULL,
 	[c_PAGECOUNT] [int] NOT NULL)
-
 GO
 
 CREATE TABLE [Movie](
@@ -18,5 +16,39 @@ CREATE TABLE [Movie](
 	[Title] [nvarchar](255) NOT NULL,
 	[ReleaseDate] [datetime] NOT NULL,
 	[Rating] [int] NULL)
+GO
 
+CREATE TABLE [Blog](
+	[Id] [int] NOT NULL PRIMARY KEY IDENTITY,
+	[Name] [nvarchar](255) NOT NULL
+)
+GO
+
+CREATE TABLE [Post](
+	[Id] [int] NOT NULL PRIMARY KEY IDENTITY,
+	[Title] [nvarchar](255) NOT NULL,
+	[Content] [nvarchar](4000) NOT NULL,
+	[PublishDate] [datetime] NOT NULL,
+	[AuthorId] [uniqueidentifier] NOT NULL,
+	[BlogId] [int] NOT NULL
+)
+GO
+
+ALTER TABLE [Post] ADD CONSTRAINT FK_Post_User FOREIGN KEY (AuthorId) REFERENCES [User] ([Id])
+GO
+ALTER TABLE [Post] ADD CONSTRAINT FK_Post_Blog FOREIGN KEY (BlogId) REFERENCES [Blog] ([Id])
+GO
+
+CREATE TABLE [Comment](
+	[Id] [int] NOT NULL PRIMARY KEY IDENTITY,
+	[Content] [nvarchar](4000) NOT NULL,
+	[PublishDate] [datetime] NOT NULL,
+	[UserId] [uniqueidentifier] NOT NULL,
+	[PostId] [int] NOT NULL
+)
+GO
+
+ALTER TABLE [Comment] ADD CONSTRAINT FK_Comment_Post FOREIGN KEY (PostId) REFERENCES [Post] ([Id])
+GO
+ALTER TABLE [Comment] ADD CONSTRAINT FK_Comment_User FOREIGN KEY (UserId) REFERENCES [User] ([Id])
 GO
