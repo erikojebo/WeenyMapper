15c1c1a319d68710b8441cb39a50b083c339d3bf
Erik Ojebo <erik.ojebo@gmail.com>
2012-03-14 20:28:18 +0100

    Added QueryException class

---
+++ src/WeenyMapper/Exceptions/QueryException.cs
@@ -0,0 +1,8 @@
+﻿namespace WeenyMapper.Exceptions
+{
+    public class QueryException : WeenyMapperException
+    {
+        public QueryException(string message) : base(message) {}
+        public QueryException(string format, params object[] values) : base(format, values) {}
+    }
+}
