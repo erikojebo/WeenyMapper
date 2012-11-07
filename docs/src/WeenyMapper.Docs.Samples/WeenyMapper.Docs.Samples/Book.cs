6cbfc6e04fed6fa72e877004e4799f3deeff4cf1
Erik Öjebo <erik.ojebo@gmail.com>
2012-11-07 14:57:33 +0100

    started implementing samples for docs

---
+++ docs/src/WeenyMapper.Docs.Samples/WeenyMapper.Docs.Samples/Book.cs
@@ -0,0 +1,9 @@
+namespace WeenyMapper.Docs.Samples
+{
+    public class Book
+    {
+        public string ISBN { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+    }
+}
