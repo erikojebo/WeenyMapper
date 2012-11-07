139ec7746f7cb79aa49ffa26ce56691f920f2e1f
Erik Öjebo <erik.ojebo@gmail.com>
2012-11-07 14:57:43 +0100

    added connection string factory methods

---
+++ src/WeenyMapper.Specs/Builders/ConnectionStringSpecs.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using WeenyMapper.Builders;
+
+namespace WeenyMapper.Specs.Builders
+{
+    [TestFixture]
+    public class ConnectionStringSpecs
+    {
+        [Test]
+        public void Connection_string_with_trusted_connection_includes_server()
+        {
+            var connectionString = ConnectionString.CreateWithTrustedConnection("SomeServer", "SomeDatabase");
+
+            Assert.That(connectionString, Is.StringContaining("Server=SomeServer;"));
+        }
+
+        [Test]
+        public void Connection_string_with_trusted_connection_includes_database()
+        {
+            var connectionString = ConnectionString.CreateWithTrustedConnection("SomeServer", "SomeDatabase");
+
+            Assert.That(connectionString, Is.StringContaining("Database=SomeDatabase;"));
+        }
+        
+        [Test]
+        public void Connection_string_with_trusted_connection_includes_Trusted_Connection()
+        {
+            var connectionString = ConnectionString.CreateWithTrustedConnection("SomeServer", "SomeDatabase");
+
+            Assert.That(connectionString, Is.StringContaining("Trusted_Connection=True;"));
+        }
+        
+        [Test]
+        public void Connection_string_with_user_id_and_password_includes_server()
+        {
+            var connectionString = ConnectionString.CreateWithUserIdAndPassword("SomeServer", "SomeDatabase", "SomeUsername", "SomePassword");
+
+            Assert.That(connectionString, Is.StringContaining("Server=SomeServer;"));
+        }
+
+        [Test]
+        public void Connection_string_with_user_id_and_password_includes_database()
+        {
+            var connectionString = ConnectionString.CreateWithUserIdAndPassword("SomeServer", "SomeDatabase", "SomeUsername", "SomePassword");
+
+            Assert.That(connectionString, Is.StringContaining("Database=SomeDatabase;"));
+        }
+        
+        [Test]
+        public void Connection_string_with_user_id_and_password_includes_user_id_and_password()
+        {
+            var connectionString = ConnectionString.CreateWithUserIdAndPassword("SomeServer", "SomeDatabase", "SomeUsername", "SomePassword");
+
+            Assert.That(connectionString, Is.StringContaining("User Id=SomeUsername;Password=SomePassword;"));
+        }
+
+    }
+}
---
+++ src/WeenyMapper/Builders/ConnectionString.cs
@@ -0,0 +1,15 @@
+﻿namespace WeenyMapper.Builders
+{
+    public class ConnectionString
+    {
+        public static string CreateWithTrustedConnection(string server, string database)
+        {
+            return string.Format("Server={0};Database={1};Trusted_Connection=True;", server, database);
+        }
+
+        public static string CreateWithUserIdAndPassword(string server, string database, string userId, string password)
+        {
+            return string.Format("Server={0};Database={1};User Id={2};Password={3};", server, database, userId, password); 
+        }
+    }
+}
